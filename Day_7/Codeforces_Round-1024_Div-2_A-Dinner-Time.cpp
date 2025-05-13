0fe08f0f72e15da735ef0ed5ba9d6352b2f1f961
setiAjiid <133765568+setiAjiid@users.noreply.github.com>
2025-05-13 21:04:33 +0700

    Update Codeforces_Round-1024_Div-2_A-Dinner-Time.cpp

--- Day_7/Codeforces_Round-1024_Div-2_A-Dinner-Time.cpp
+++ Day_7/Codeforces_Round-1024_Div-2_A-Dinner-Time.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define ll long long
 #define ull unsigned long long
 #define el '\n'
-#define day_6 ios::sync_with_stdio(0), cin.tie(0)
+#define day_7 ios::sync_with_stdio(0), cin.tie(0)
 
 //#include <ext/pb_ds/assoc_container.hpp>
 //#include <ext/pb_ds/tree_policy.hpp>
@@ -16,7 +16,7 @@ using namespace std;
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main(){
-    day_6;
+    day_7;
     int t;
     cin >> t;
     while (t--){
